69fb28dead85da8691cb899c8e086024e10727a0
Orlando Groppo <Orlando@GGs.lan>
2020-05-10 12:02:42 +0100

    startup updates

--- server/main.js
+++ server/main.js
@@ -1,2 +1,8 @@
 import { Meteor } from "meteor/meteor";
 import "./publications";
+import "./methods";
+import "./migrations";
+
+Meteor.startup(() => {
+  Migrations.migrateTo('latest');
+})
---
+++ server/migrations/templateMigration.js
@@ -0,0 +1,6 @@
+Migrations.add({
+  version: 0,
+  name: 'parse collins csv file',
+  up: function() {
+  }
+});
